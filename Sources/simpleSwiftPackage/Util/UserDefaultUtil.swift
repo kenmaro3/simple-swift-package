3e532f086814fe5e8b5a9ec78b17a2b2b2536c78
kenmaro3 <miha.ken.19@gmail.com>
2023-10-10 18:47:54 +0900

    [UPD] agreement request

--- Sources/simpleSwiftPackage/Util/UserDefaultUtil.swift
+++ Sources/simpleSwiftPackage/Util/UserDefaultUtil.swift
@@ -9,37 +9,37 @@ import Foundation
 
 func setName(_ value: String){
     let userDefaults = UserDefaults.standard
-    userDefaults.set(value, forKey: "personal_name")
+    userDefaults.set(value, forKey: "authblue_personal_name")
 }
 
 func setBirthday(_ value: String){
     let userDefaults = UserDefaults.standard
-    userDefaults.set(value, forKey: "personal_birthday")
+    userDefaults.set(value, forKey: "authblue_personal_birthday")
 }
 
 func setAge(_ value: String){
     let userDefaults = UserDefaults.standard
-    userDefaults.set(value, forKey: "personal_age")
+    userDefaults.set(value, forKey: "authblue_personal_age")
 }
 
 func setSex(_ value: String){
     let userDefaults = UserDefaults.standard
-    userDefaults.set(value, forKey: "personal_sex")
+    userDefaults.set(value, forKey: "authblue_personal_sex")
 }
 
 func setAddress(_ value: String){
     let userDefaults = UserDefaults.standard
-    userDefaults.set(value, forKey: "personal_address")
+    userDefaults.set(value, forKey: "authblue_personal_address")
 }
 
 func setPhone(_ value: String){
     let userDefaults = UserDefaults.standard
-    userDefaults.set(value, forKey: "personal_phone")
+    userDefaults.set(value, forKey: "authblue_personal_phone")
 }
 
 func setEmail(_ value: String){
     let userDefaults = UserDefaults.standard
-    userDefaults.set(value, forKey: "personal_email")
+    userDefaults.set(value, forKey: "authblue_personal_email")
 }
 
 func setIsOverlayVisible(_ value: Bool){
